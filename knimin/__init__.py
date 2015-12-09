53d202f994c5dd94bd81ef08da7c13f2fda8d349
Joshua Shorenstein <squirrelo@gmail.com>
2015-12-08 16:19:05 -0800

    move Transaction object over

--- knimin/__init__.py
+++ knimin/__init__.py
@@ -2,8 +2,9 @@
 
 from knimin.lib.configuration import KniminConfig
 from knimin.lib.data_access import KniminAccess
+from knimin.lib.sql_connection import TRN
 
 config = KniminConfig()
 db = KniminAccess(config)
 
-__all__ = ['config', 'db']
+__all__ = ['config', 'db', 'TRN']
